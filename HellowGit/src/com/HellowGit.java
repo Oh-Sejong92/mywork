57ca0582122fc065052e2dd7b991a8ade5ca4bf8
user44 <user44@gntp-44.gntp.local>
2018-04-27 14:46:03 +0900

    키보드로 이름 입력 받기 추가

--- HellowGit/src/com/HellowGit.java
+++ HellowGit/src/com/HellowGit.java
@@ -1,9 +1,13 @@
 package com;
-
+import java.util.*;
 public class HellowGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hellow");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.nextLine();
 	}
 
 }
