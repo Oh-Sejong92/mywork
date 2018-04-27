7f800cb4dd8c580333f2297b06a11eafb61109f9
user44 <user44@gntp-44.gntp.local>
2018-04-27 15:42:58 +0900

    시작

---
+++ ArrayListTest/src/com/ArrayListEx.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListEx {
+
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<String>();
+		
+		Scanner sc = new Scanner(System.in);
+		for(int i=0;i<4;i++) {
+			System.out.println("이름을 입력하세요>>");
+			String s = sc.next();
+			a.add(s);
+		}
+		
+		for(int i=0;i<a.size();i++) {
+			String name = a.get(i);
+			System.out.println(name+" ");
+		}
+		int longesrIndex=0;
+		for(int i=1;i<a.size();i++) {
+			if(a.get(longesrIndex).length()<a.get(i).length())
+				longesrIndex = i;
+		}
+		System.out.println("\n가장 긴 이름은 : "+a.get(longesrIndex));
+		sc.close();
+	}
+	
+
+}
